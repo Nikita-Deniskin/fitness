da24a574b35cc7c2f177dd6062dc29b0c811d227
Михаил <129779510+KiroiiKitsune@users.noreply.github.com>
2023-11-15 22:12:37 +0300

    Login_password

---
+++ course-app/src/helpers/new-password/new.password.js
@@ -0,0 +1,25 @@
+import LogoSvg from "../svg-img/logo-svg/logoSvg";
+import "../loginpage/login.css"
+
+
+
+export default function NewPassword () {
+    return (
+        <div className="login">
+        <div className="login__box password">
+            <LogoSvg/>
+        
+          <div className="login__text">
+            <h3>Новый пароль:</h3>
+          </div>
+    
+          <div className="login__form login__form-new">
+            <input placeholder="Проль" type="password" className="login__form-text"/>
+          <input placeholder="Повторите пароль" type="password" className="login__form-text"/>
+          </div>
+          <button className="get__btn_new  __btn" type="button">Сохранить</button>
+        </div>
+        
+    </div>
+    )
+}
